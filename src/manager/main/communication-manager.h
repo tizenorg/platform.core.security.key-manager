f27b8dc140bc0d6f935765076dd952b2787e49ff
Krzysztof Jackiewicz <k.jackiewicz@samsung.com>
2015-03-18 17:10:30 +0100

    Add inter-service communication framework
    [Issue#] N/A
    [Feature/Bug] N/A
    [Problem] Services need to communicate with each other
    [Cause] N/A
    [Solution] Framework for inter-service communication added.
    
    [Verification] Run ckm-tests-internal -t MESSAGE_MANAGER_TEST
    
    Change-Id: I28714ba52efe25c47402adb6ac1bef52859ed898

---
+++ src/manager/main/communication-manager.h
@@ -0,0 +1,88 @@
+/*
+ *  Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+/*
+ * @file       communication-manager.h
+ * @author     Krzysztof Jackiewicz (k.jackiewicz@samsung.com)
+ * @version    1.0
+ */
+
+#pragma once
+
+#include <functional>
+#include <list>
+
+namespace CKM {
+
+/*
+ * class responsible for keeping a list of listeners for given M type of message and notifying them
+ */
+template <typename M>
+class MessageManager
+{
+public:
+    // Listener is an object callable with const M& as argument
+    template <typename L>
+    void Register(L&& listener)
+    {
+        m_handlers.push_back(std::move(listener));
+    }
+
+    // Sends message of type M to all registered listeners
+    void SendMessage(const M& msg)
+    {
+        for(auto it : m_handlers)
+            it(msg);
+    }
+
+private:
+    std::list<std::function<void(const M&)>> m_handlers;
+};
+
+// generic template declaration
+template <typename... Args>
+struct CommunicationManager;
+
+/*
+ * Class that combines MessageManagers of all requested Message types into a single object. Examples
+ * can be found in tests (test_msg-manager.cpp)
+ */
+template <typename First, typename... Args>
+struct CommunicationManager<First, Args...> :
+    public MessageManager<First>, public CommunicationManager<Args...>
+{
+public:
+
+    // M - message type, L - listener to register
+    template <typename M, typename L>
+    void Register(L&& listener)
+    {
+        MessageManager<M>::Register(std::move(listener));
+    }
+
+    // M message type
+    template <typename M>
+    void SendMessage(const M& msg)
+    {
+        MessageManager<M>::SendMessage(msg);
+    }
+};
+
+// stop condition for recursive inheritance
+template <>
+struct CommunicationManager<> {
+};
+
+} /* namespace CKM */
