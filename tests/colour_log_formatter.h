5372b874fb7052e19258724a194c9acadf04afb0
Kyungwook Tak <k.tak@samsung.com>
2016-01-04 16:46:57 +0900

    Add license header on tests srcs
    Change-Id: I9603ef02dec7d541ab0cbd13768ebc8fb0bdc12b
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

--- tests/colour_log_formatter.h
+++ tests/colour_log_formatter.h
@@ -1,6 +1,26 @@
-#ifndef COLOUR_LOG_FORMATTER_H_
-#define COLOUR_LOG_FORMATTER_H_
-
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Contact: Kyungwook Tak <k.tak@samsung.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ * @file        colour_log_formatter.h
+ * @author      Zofia Abramowska (z.abramowska@samsung.com)
+ * @version
+ * @brief
+ */
+#pragma once
 #include <boost/test/unit_test_log_formatter.hpp>
 
 namespace CKM {
@@ -45,5 +65,3 @@ private:
     bool m_isTestCaseFailed;
 };
 } // namespace CKM
-
-#endif /* COLOUR_LOG_FORMATTER_H_ */
